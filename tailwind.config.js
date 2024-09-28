caf5f24bf8ea2c76b761dba8a1f036724d7be490
penn <penn201500@gmail.com>
2024-09-28 10:26:34 +0800

    custom screen sizes and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
     content: ["./*.html"],
     theme: {
-        extend: {},
+        screens: {
+            sm: "480px",
+            md: "78px",
+            lg: "976px",
+            xl: "1440px",
+        },
+        extend: {
+            colors: {
+                strongCyan: "hsl(171, 66%, 44%)",
+                lightBlue: "hsl(233, 100%, 69%)",
+                darkGrayishBlue: "hsl(210, 10%, 33%)",
+                grayishBlue: "hsl(201, 11%, 66%)",
+            },
+        },
     },
     plugins: [],
 }
